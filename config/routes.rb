dce3f0f0d2dd300b87ae541407b568eed971e3d2
Kris Birov <kris.birov@gmail.com>
2021-11-30 15:18:33 +0100

    Add locale scope to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Spree::Core::Engine.add_routes do
-  namespace :admin do
-    resources :reviews, only: [:index, :destroy, :edit, :update] do
-      member do
-        get :approve
+  scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
+    namespace :admin do
+      resources :reviews, only: [:index, :destroy, :edit, :update] do
+        member do
+          get :approve
+        end
+        resources :feedback_reviews, only: [:index, :destroy]
       end
-      resources :feedback_reviews, only: [:index, :destroy]
+      resource :review_settings, only: [:edit, :update]
     end
-    resource :review_settings, only: [:edit, :update]
-  end
 
-  resources :products, only: [] do
-    resources :reviews, only: [:index, :new, :create] do
+    resources :products, only: [] do
+      resources :reviews, only: [:index, :new, :create] do
+      end
     end
+    post '/reviews/:review_id/feedback(.:format)' => 'feedback_reviews#create', as: :feedback_reviews
   end
-  post '/reviews/:review_id/feedback(.:format)' => 'feedback_reviews#create', as: :feedback_reviews
 end
